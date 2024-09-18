c11ec0a7ed1cbcac84333b597e380b2544205541
patolinbr <a01781663@tec.mx>
2024-09-17 20:36:07 -0600

    create hello world

---
+++ dataframe.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+myDataframe = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]}) #recibe json con esta estructura, columna "yes" y columna "no"
+
+print(myDataframe)
---
+++ series.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+mySeries = pd.Series([2,3,4,5,6])
+
+print(mySeries)
+
